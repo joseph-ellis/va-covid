cd032eba90312b2faea8c640a80ba91aef53cb21
Joseph <joseph.ellis@ellisanalytics.com>
2020-10-28 11:27:46 +0000

    Quick Refactor
    Refactored data transformation to implement DRY.

--- R/DataTransform.R
+++ R/DataTransform.R
@@ -62,96 +62,55 @@ allLocalMetricsByDate <- vdoh_data$CasesLocality %>%
     )
   )
 
+# operations for summary objects
+SummarizeMetrics <- function(.data) {
+  .data %>%
+    summarize(
+      Population2019 = sum(Population2019),
+      TotalCases = sum(TotalCases),
+      Hospitalizations = sum(Hospitalizations),
+      Deaths = sum(Deaths)
+    ) %>%
+    mutate(
+      CasesPerCapita = TotalCases / Population2019,
+      CasesPer100K = CasesPerCapita * 1e5,
+      PercentCasesHospitalized = Hospitalizations / TotalCases,
+      HospitalizationPerCapita = Hospitalizations / Population2019,
+      HospitalizationsPer100K = HospitalizationPerCapita * 1e5,
+      MortalityRate = Deaths / TotalCases,
+      DeathsPerCapita = Deaths / Population2019,
+      DeathsPer100K = DeathsPerCapita * 1e5,
+      across(
+        .cols = where(is.numeric),
+        .fns = ~ round(
+          .x,
+          4
+        )
+      )
+    )
+}
+
 # summary of current metrics for entire state
 stateCurrentMetricSummary <- allLocalMetricsByDate %>%
   filter(Date == max(Date)) %>%
   group_by(Date) %>%
-  summarize(
-    Population2019 = sum(Population2019),
-    TotalCases = sum(TotalCases),
-    Hospitalizations = sum(Hospitalizations),
-    Deaths = sum(Deaths)
-  ) %>%
-  mutate(
-    CasesPerCapita = TotalCases / Population2019,
-    CasesPer100K = CasesPerCapita * 1e5,
-    PercentCasesHospitalized = Hospitalizations / TotalCases,
-    HospitalizationPerCapita = Hospitalizations / Population2019,
-    HospitalizationsPer100K = HospitalizationPerCapita * 1e5,
-    MortalityRate = Deaths / TotalCases,
-    DeathsPerCapita = Deaths / Population2019,
-    DeathsPer100K = DeathsPerCapita * 1e5,
-    across(
-      .cols = where(is.numeric),
-      .fns = ~ round(
-        .x,
-        4
-      )
-    )
-  )
+  SummarizeMetrics()
 
 # summary of current metrics for health planning regions
 healthPlanningRegionCurrentMetricSummary <- allLocalMetricsByDate %>%
   filter(Date == max(Date)) %>%
   group_by(Date, HealthPlanningRegion) %>%
-  summarize(
-    Population2019 = sum(Population2019),
-    TotalCases = sum(TotalCases),
-    Hospitalizations = sum(Hospitalizations),
-    Deaths = sum(Deaths)
-  ) %>%
-  mutate(
-    CasesPerCapita = TotalCases / Population2019,
-    CasesPer100K = CasesPerCapita * 1e5,
-    PercentCasesHospitalized = Hospitalizations / TotalCases,
-    HospitalizationPerCapita = Hospitalizations / Population2019,
-    HospitalizationsPer100K = HospitalizationPerCapita * 1e5,
-    MortalityRate = Deaths / TotalCases,
-    DeathsPerCapita = Deaths / Population2019,
-    DeathsPer100K = DeathsPerCapita * 1e5,
-    across(
-      .cols = where(is.numeric),
-      .fns = ~ round(
-        .x,
-        4
-      )
-    )
-  )
+  SummarizeMetrics()
 
 # summary of current metrics for health districts
 healthDistrictCurrentMetricSummary <- allLocalMetricsByDate %>%
   filter(Date == max(Date)) %>%
   group_by(Date, HealthPlanningRegion, HealthDistrict) %>%
-  summarize(
-    Population2019 = sum(Population2019),
-    TotalCases = sum(TotalCases),
-    Hospitalizations = sum(Hospitalizations),
-    Deaths = sum(Deaths)
-  ) %>%
-  mutate(
-    CasesPerCapita = TotalCases / Population2019,
-    CasesPer100K = CasesPerCapita * 1e5,
-    PercentCasesHospitalized = Hospitalizations / TotalCases,
-    HospitalizationPerCapita = Hospitalizations / Population2019,
-    HospitalizationsPer100K = HospitalizationPerCapita * 1e5,
-    MortalityRate = Deaths / TotalCases,
-    DeathsPerCapita = Deaths / Population2019,
-    DeathsPer100K = DeathsPerCapita * 1e5,
-    across(
-      .cols = where(is.numeric),
-      .fns = ~ round(
-        .x,
-        4
-      )
-    )
-  )
+  SummarizeMetrics()
 
 # summary of current metrics for localities
 localityCurrentMetricSummary <- allLocalMetricsByDate %>%
   filter(Date == max(Date))
-
-
-
 
 # objects to save
 toSave <- c("stateCurrentMetricSummary",
